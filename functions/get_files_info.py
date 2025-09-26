b79cfcb5ed56560ee8863657f43ab393ccbe913e
Drtrider <drtrider200@gmail.com>
2025-09-26 07:38:42 -0700

    Working version that passed the tests!!

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,21 +6,27 @@ def get_files_info(working_directory, directory="."):
     target_path_abs = os.path.abspath(target_path)
     working_directory_abs = os.path.abspath(working_directory)
 
-    # Check if directory is a valid directory
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
+    # Check if directory is a valid directory, return error string otherwise
+    if not os.path.isdir(target_path_abs):
+        return f'Error: "{target_path_abs}" is not a directory'
     
-    # If the path of directory is outside the working_directory, throw an error
-    if target_path_abs in working_directory_abs:
+    # If the path of directory is outside the working_directory, return an error
+    if target_path_abs.startswith(working_directory_abs):
         return_list = []
 
         target_path_content = os.listdir(target_path_abs)
         for item in target_path_content:
-            item_dir_status = os.path.isdir(item)
-            item_file_size = os.path.getsize(item)
+            item_path_abs = os.path.join(target_path_abs, item)   
+            item_dir_status = os.path.isdir(item_path_abs)
+            item_file_size = os.path.getsize(item_path_abs)
             item_detail_string = f"- {item}: file_size={item_file_size} bytes, is_dir={item_dir_status}"
+            
+            if item.startswith("__"):
+                continue
+            else:
+                return_list.append(item_detail_string)
 
-        pass
+        return return_list
 
     else:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
--- tests.py
+++ tests.py
@@ -1,5 +1,31 @@
 from functions.get_files_info import get_files_info
 
-return_data = get_files_info("calculator", ".")
+return_data_test_1 = get_files_info("calculator", ".")
 
-print(return_data)
+if "Error" in return_data_test_1:
+    print(return_data_test_1)
+else:
+    print(f"Result for currrent directory:")
+    [print(item) for item in return_data_test_1]
+
+return_data_test_2 = get_files_info("calculator", "pkg")
+
+if "Error" in return_data_test_2:
+    print(return_data_test_2)
+else:
+    print(f"Result for 'pkg' directory:")
+    [print(item) for item in return_data_test_2]
+
+return_data_test_3 = get_files_info("calculator", "/bin")
+if "Error" in return_data_test_3:
+    print(return_data_test_3)
+else:
+    print(f"Result for 'pkg' directory:")
+    [print(item) for item in return_data_test_3]
+
+return_data_test_4 = get_files_info("calculator", "../")
+if "Error" in return_data_test_4:
+    print(return_data_test_4)
+else:
+    print(f"Result for 'pkg' directory:")
+    [print(item) for item in return_data_test_4]
